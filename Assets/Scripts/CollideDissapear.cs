4c4f370fbd8b037be58ad5e66ec6d42f6030223d
Takuro Takezawa <t910inicident@gmail.com>
2019-07-28 10:51:19 +0900

    Initial commit

---
+++ Assets/Scripts/CollideDissapear.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideDissapear : MonoBehaviour
+{
+    GameObject fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.localPosition.y > this.transform.localPosition.y)
+		{
+			
+			Instantiate(fire, this.transform);
+			// fire.transform.parent = this.gameObject.transform;
+			Destroy(this.gameObject);
+			DestroyImmediate(fire, true);
+		}
+    }
+}
